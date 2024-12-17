88ba066c1ba0a6505a8668cf5ef3e73650ee29e7
Caio Fiszuk <caiolopes871@gmail.com>
2024-12-16 21:56:52 -0300

    teste

--- frontend/src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-function Footer() {
-    return (
-        <footer className="footer">&copy; 2021 Around The U.S.</footer>
-    );
-}
-
-export default Footer;
--- frontend/src/components/Validator.jsx
+++
@@ -1,7 +0,0 @@
-function Validator({message}) {
-    return(
-        <span className="popup__container-error">{message}</span>
-    );
-}
-
-export default Validator;
